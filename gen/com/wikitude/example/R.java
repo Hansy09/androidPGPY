438b81b08f9d6c9b28712f784413e37f33e4e508
Shago <Shago@SMC1.lan>
2013-01-28 20:45:08 -0600

    Interfaz de Usuario, con botones para la lista, para Buscar, el cuadro de texto para insertar lo que se quiere buscar, el controlador de lo que sería el rango del radar

--- gen/com/wikitude/example/R.java
+++ gen/com/wikitude/example/R.java
@@ -15,13 +15,16 @@ public final class R {
     }
     public static final class id {
         public static final int architectView=0x7f050000;
-        public static final int linearLayout1=0x7f050002;
-        public static final int linearLayout2=0x7f050005;
-        public static final int textView1=0x7f050003;
-        public static final int textView2=0x7f050006;
-        public static final int textView3=0x7f050001;
-        public static final int tvPoiDesc=0x7f050007;
-        public static final int tvPoiName=0x7f050004;
+        public static final int botonLista=0x7f050001;
+        public static final int linearLayout1=0x7f050005;
+        public static final int linearLayout2=0x7f050008;
+        public static final int seekBar1=0x7f050003;
+        public static final int textBuscar=0x7f050002;
+        public static final int textView1=0x7f050006;
+        public static final int textView2=0x7f050009;
+        public static final int textView3=0x7f050004;
+        public static final int tvPoiDesc=0x7f05000a;
+        public static final int tvPoiName=0x7f050007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,5 +32,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int botonBuscar=0x7f040003;
+        public static final int botonLista=0x7f040002;
+        public static final int textBuscar=0x7f040001;
     }
 }
