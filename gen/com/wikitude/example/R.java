2bad6c8bb62231e6b9397870ab3f318954aec88b
Hansy <Hansy@Hansy-PC.mshome.net>
2013-02-17 13:44:05 -0600

    odio git

--- gen/com/wikitude/example/R.java
+++ gen/com/wikitude/example/R.java
@@ -39,9 +39,11 @@ public final class R {
         public static final int tvPoiName=0x7f060012;
     }
     public static final class layout {
-        public static final int activity_pdidetalle=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int poi_detail=0x7f030002;
+        public static final int activity_busqueda_simple=0x7f030000;
+        public static final int activity_pdidetalle=0x7f030001;
+        public static final int activity_visualizar_lista=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int poi_detail=0x7f030004;
     }
     public static final class menu {
         public static final int activity_pdidetalle=0x7f050000;
